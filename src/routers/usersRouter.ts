065a3dfcc088fe60366bd914dcb963ecac9956ec
harishvalluru2203 <harish.valluru22@outlook.com>
2024-02-05 00:42:01 +0530

    Error Handling Implemented

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -4,37 +4,64 @@ import { User } from "../models/users.model";
 export const usersRouter = Router();
 
 usersRouter.get("/list", async (req, res) => {
-  const users = await User.find();
-  res.send(users);
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 usersRouter.get("/:id", async (req, res) => {
-  const userId = req.params.id;
-  const userDetails = await User.findById(userId);
-  res.send(userDetails);
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 usersRouter.post("/registration", async (req, res) => {
-  const userDetails = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName,
-  });
-  const response = await userDetails.save();
-  res.json(response);
+  try {
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+    });
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
 
 usersRouter.put("/update", async (req, res) => {
-  const userToUpdate = req.body._id;
-  const userDetails = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName,
-  };
-  const response = await User.findOneAndUpdate(
-    { _id: userToUpdate },
-    { $set: userDetails },
-    { new: true }
-  );
-  res.send(response);
+  try {
+    const userToUpdate = req.body._id;
+    const user = await User.findByIdAndUpdate(userToUpdate, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+usersRouter.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
 });
