0bc3820f58ce9c0b8d04a69f1297277372109967
harishvalluru2203 <harish.valluru22@outlook.com>
2024-02-06 02:36:05 +0530

    password endoded again

--- src/server.ts
+++ src/server.ts
@@ -44,7 +44,7 @@ app.listen("4000", () => {
 });
 
 const uri =
-  "mongodb+srv://harishvalluru22:Samvi@2203@complaint-box-cluster.jobsjmz.mongodb.net/?retryWrites=true&w=majority&directConnection=true";
+  "mongodb+srv://harishvalluru22:Samvi%402203@complaint-box-cluster.jobsjmz.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.MONGODB_CLOUD_URI, {
